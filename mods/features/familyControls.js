8b973e5b80ad83be266b8bb453e397ab7cdfc052
SolidFinancialHabits <solidfinancialhabits@proton.me>
2026-07-14 13:19:38 -0400

    Connect Family Controls to saved settings

--- mods/features/familyControls.js
+++ mods/features/familyControls.js
@@ -1,1 +1,10 @@
-console.log("[TizenTube Family Controls] Loaded");
+import { configRead } from '../config.js';
+
+const isKidsMode = () => configRead('familyKidsMode');
+
+console.log('[TizenTube Family Controls] Loaded', {
+  kidsMode: isKidsMode(),
+  blockedKeywords: configRead('familyBlockedKeywords'),
+  removeShorts: configRead('familyRemoveShorts'),
+  disableAutoplay: configRead('familyDisableAutoplay')
+});
